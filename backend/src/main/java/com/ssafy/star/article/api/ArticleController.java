14c8e9d8f87407e0a3ad03848b21e58f83ff62ed
이창헌 <changbill@naver.com>
2024-02-05 17:48:49 +0900

    #20 - feat : article - comment연결, constellation연결, 휴지통 기능
    article commit 입니다
    
    Change-Id: If237d296f2798a94921c33351240d4a868bf1e8e

--- backend/src/main/java/com/ssafy/star/article/api/ArticleController.java
+++ backend/src/main/java/com/ssafy/star/article/api/ArticleController.java
@@ -3,7 +3,9 @@ package com.ssafy.star.article.api;
 
 import com.ssafy.star.article.application.ArticleService;
 import com.ssafy.star.article.dto.Article;
+import com.ssafy.star.article.dto.request.ArticleConstellationSelect;
 import com.ssafy.star.article.dto.request.ArticleCreateRequest;
+import com.ssafy.star.article.dto.request.ArticleDeletionUndo;
 import com.ssafy.star.article.dto.request.ArticleModifyRequest;
 import com.ssafy.star.article.dto.response.ArticleResponse;
 import com.ssafy.star.article.dto.response.Response;
@@ -31,7 +33,7 @@ import org.springframework.web.bind.annotation.*;
 public class ArticleController {
 
     private final ArticleService articleService;
-
+    //TODO : 주소에서 articles 위치 변경
 
     @PostMapping
     public Response<Void> create(@RequestBody ArticleCreateRequest request, Authentication authentication) {
@@ -51,7 +53,6 @@ public class ArticleController {
     )
     @PutMapping("/{articleId}")
     public Response<ArticleResponse> modify(@PathVariable Long articleId, @RequestBody ArticleModifyRequest request, Authentication authentication) {
-        // TODO : image
         Article article = articleService.modify(articleId, request.title(), request.tag(), request.description(),
                 request.disclosureType(), authentication.getName());
         return Response.success(ArticleResponse.fromArticle(article));
@@ -63,7 +64,6 @@ public class ArticleController {
         return Response.success();
     }
 
-    // TODO: 검색 시 전체 조회 필요할듯
     @Operation(
             summary = "게시물 전체 조회",
             description = "게시물 전체 조회입니다.",
@@ -102,4 +102,48 @@ public class ArticleController {
 
         return Response.success(ArticleResponse.fromArticle(articleService.detail(articleId, email)));
     }
+
+    @Operation(
+            summary = "별자리 배정",
+            description = "게시물에 별자리를 배정합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "배정 성공", content = @Content(schema = @Schema(implementation = ArticleResponse.class)))
+            }
+    )
+    @PostMapping("{articleId}/constellation-select")
+    public Response<Void> select(@PathVariable Long articleId, @RequestBody ArticleConstellationSelect articleConstellationSelect, Authentication authentication) {
+        articleService.select(articleId, articleConstellationSelect.constellationId(), authentication.getName());
+        return Response.success();
+    }
+
+    // 별자리 Id로 특정된 별자리의 전체 게시물 조회
+    @GetMapping("/constellation/{constellationId}")
+    public Response<Page<ArticleResponse>> articlesInConstellation(@PathVariable Long constellationId, Authentication authentication, Pageable pageable) {
+        return Response.success(articleService.articlesInConstellation(constellationId, authentication.getName(), pageable).map(ArticleResponse::fromArticle));
+    }
+
+    @Operation(
+            summary = "휴지통 조회",
+            description = "휴지통을 조회합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = ArticleResponse.class)))
+            }
+    )
+    @GetMapping("/trashcan")
+    public Response<Page<ArticleResponse>> trashcan(Authentication authentication, Pageable pageable) {
+        return Response.success(articleService.trashcan(authentication.getName(), pageable).map(ArticleResponse::fromArticle));
+    }
+
+    @Operation(
+            summary = "게시물 복원",
+            description = "휴지통에 있던 게시물을 복원합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "복원 성공", content = @Content(schema = @Schema(implementation = ArticleResponse.class)))
+            }
+    )
+    @PutMapping("/trashcan/undo")
+    public Response<ArticleResponse> undoDeletion(@RequestBody ArticleDeletionUndo articleDeletionUndo, Authentication authentication) {
+        return Response.success(ArticleResponse.fromArticle(articleService.undoDeletion(articleDeletionUndo.articleId(),authentication.getName())));
+    }
+
 }
--- backend/src/main/java/com/ssafy/star/article/application/ArticleService.java
+++ backend/src/main/java/com/ssafy/star/article/application/ArticleService.java
@@ -6,6 +6,8 @@ import com.ssafy.star.article.domain.ArticleEntity;
 import com.ssafy.star.article.dto.Article;
 import com.ssafy.star.common.exception.ByeolDamException;
 import com.ssafy.star.common.exception.ErrorCode;
+import com.ssafy.star.constellation.dao.ConstellationRepository;
+import com.ssafy.star.constellation.domain.ConstellationEntity;
 import com.ssafy.star.user.domain.UserEntity;
 import com.ssafy.star.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,35 +17,44 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+    private final ConstellationRepository constellationRepository;
     private final UserRepository userRepository;
 
     // 게시물 등록
     @Transactional
-    public void create(String title, String tag, String description, DisclosureType disclosureType,
-                       String email) {
+    public void create(
+            String title,
+            String tag,
+            String description,
+            DisclosureType disclosureType,
+            String email
+    ) {
         UserEntity userEntity = getUserEntityOrException(email);
-        log.info("userEntity 정보 : {}", userEntity);
-        articleRepository.save(ArticleEntity.of(title, tag, description, disclosureType, userEntity));
-    }   // user 객체가 하는 역할이 뭔지?
+
+        // constellationEntity가 null인 경우 별자리 미분류 게시물
+        articleRepository.save(ArticleEntity.of(title, tag, description, disclosureType, userEntity, null));
+    }
 
     // 게시물 수정
     @Transactional
-    public Article modify(Long articleId, String title, String tag, String description, DisclosureType disclosure,
-                          String email) {
-
-        ArticleEntity articleEntity = getArticleEntityOrException(articleId);
-        UserEntity userEntity = getUserEntityOrException(email);
-
-        if (articleEntity.getUser() != userEntity) {
-            throw new ByeolDamException(ErrorCode.INVALID_PERMISSION,
-                    String.format("%s has no permission with %s", email, articleId));
-        }
+    public Article modify(
+            Long articleId,
+            String title,
+            String tag,
+            String description,
+            DisclosureType disclosure,
+            String email
+    ) {
+        // 게시물 owner가 맞는지 확인
+        ArticleEntity articleEntity = getArticleOwnerOrException(articleId, email);
 
         articleEntity.setTitle(title);
         articleEntity.setTag(tag);
@@ -55,59 +66,134 @@ public class ArticleService {
     // 게시물 삭제
     @Transactional
     public void delete(Long articleId, String email) {
+        // 게시물 owner가 맞는지 확인
+        ArticleEntity articleEntity = getArticleOwnerOrException(articleId, email);
+        articleRepository.delete(articleEntity);
+    }
+
+    @Transactional
+    public Page<Article> trashcan(String email, Pageable pageable) {
+        UserEntity userEntity = getUserEntityOrException(email);
+        return articleRepository.findAllByOwnerEntityAndDeleted(userEntity, pageable).map(Article::fromEntity);
+    }
+
+    @Transactional
+    public Article undoDeletion(Long articleId, String email) {
         ArticleEntity articleEntity = getArticleEntityOrException(articleId);
         UserEntity userEntity = getUserEntityOrException(email);
-        // 삭제하려는 사람이 포스트 작성한 사람인지 확인
-        if (articleEntity.getUser() != userEntity) {
-            throw new ByeolDamException(ErrorCode.INVALID_PERMISSION,
-                    String.format("%s has no permission with %s", email, articleId));
+
+        if(!articleEntity.getOwnerEntity().equals(userEntity)) {
+            throw new ByeolDamException(ErrorCode.INVALID_PERMISSION, String.format("%s has no permission", "email:" + email));
         }
-        articleRepository.delete(articleEntity);
+
+        if(articleEntity.getDeletedAt() != null) {
+            articleEntity.undoDeletion();
+        } else {
+            throw new ByeolDamException(ErrorCode.INVALID_REQUEST, String.format("%s is not abandoned", "articleId:" + Long.toString(articleId)));
+        }
+
+        return Article.fromEntity(articleEntity);
     }
 
     // 게시물 전체 조회
     @Transactional(readOnly = true)
     public Page<Article> list(String email, Pageable pageable) {
         UserEntity userEntity = getUserEntityOrException(email);
-        // DisclosureType이 VISIBLE인 ArticleEntity, 자신의 게시물 보여주기
-        return articleRepository.findArticlesForUser(userEntity, pageable).map(Article::fromEntity);
+        // Not deleted 상태이고, DisclosureType이 VISIBLE이거나 자신의 게시물이라면 보여주기
+        return articleRepository.findAllByNotDeletedAndDisclosure(userEntity, pageable).map(Article::fromEntity);
     }
 
     // 내 게시물 전체 조회
     @Transactional(readOnly = true)
     public Page<Article> my(String email, Pageable pageable) {
         UserEntity userEntity = getUserEntityOrException(email);
-        return articleRepository.findAllByUser(userEntity, pageable).map(Article::fromEntity);
+        return articleRepository.findAllByOwnerEntityAndNotDeleted(userEntity, pageable).map(Article::fromEntity);
     }
 
     // 게시물 상세 조회
     @Transactional(readOnly = true)
     public Article detail(Long articleId, String email) {
-        // 해당 article이 없을 경우 예외처리
-        ArticleEntity articleEntity = articleRepository.findById(articleId).orElseThrow(() ->
-                new ByeolDamException(ErrorCode.ARTICLE_NOT_FOUND, String.format("%s not founded", articleId)));
-
-        // DisclosureType 예외처리
         UserEntity userEntity = getUserEntityOrException(email);
-        if (articleEntity.getUser() != userEntity && articleEntity.getDisclosure() != DisclosureType.VISIBLE) {
-            throw new ByeolDamException(ErrorCode.INVALID_PERMISSION, String.format("%s has no permission with %s", email, articleId));
+
+        // 해당 article이 없을 경우 예외처리
+        ArticleEntity articleEntity = articleRepository.findById(articleId)
+                .orElseThrow(() ->
+                        new ByeolDamException(ErrorCode.ARTICLE_NOT_FOUND, String.format("%s not founded", "articleId:" + Long.toString(articleId))));
+        System.out.println("articleEntity: "+articleEntity.toString());
+
+        // articleId, Not deleted, 내 게시물이거나 VISIBLE
+        if(articleRepository.findByArticleIdAndNotDeleted(articleId, userEntity)) {
+            articleEntity.setHits(articleEntity.getHits() + 1);
+
+            return Article.fromEntity(articleRepository.save(articleEntity));
+        } else {
+            // Deletion 예외처리
+            if(articleEntity.getDeletedAt() != null) {
+                throw new ByeolDamException(ErrorCode.ARTICLE_DELETED, String.format("%s deleted", "articleId:" + Long.toString(articleId)));
+            }
+            // DisclosureType 예외처리
+//            if (articleEntity.getOwnerEntity() != userEntity && articleEntity.getDisclosure() != DisclosureType.VISIBLE)
+                throw new ByeolDamException(ErrorCode.INVALID_PERMISSION, String.format("%s has no permission with %s", "email:"+email, "articleId:" + Long.toString(articleId)));
         }
+    }
 
-        articleEntity.setHits(articleEntity.getHits() + 1);
+    // 별자리 배정, 변경
+    @Transactional
+    public void select(Long articleId, Long constellationId, String email) {
+        ArticleEntity articleEntity = getArticleOwnerOrException(articleId, email);
+        ConstellationEntity constellationEntity = getConstellationEntityOrException(constellationId);
 
-        return Article.fromEntity(articleRepository.save(articleEntity));
+        // 선택한 별자리가 기존의 constellationEntity인 경우 Error 반환
+        if(articleEntity.getConstellationEntity() == null) {
+
+        } else if (articleEntity.getConstellationEntity().equals(constellationEntity)) {
+            throw new ByeolDamException(ErrorCode.INVALID_REQUEST, String.format("%s is already constellation %s", "articleId:" + Long.toString(articleId), "constellationId:" + Long.toString(constellationId)));
+        }
+        articleEntity.setConstellationEntity(constellationEntity);
+    }
+
+    // 별자리 Id로 특정된 별자리의 전체 게시물 조회
+    @Transactional
+    public Page<Article> articlesInConstellation(Long constellationId, String email, Pageable pageable) {
+        // email로 userEntity 구하고 별자리 공개여부와 해당 게시물 공유여부를 확인해 Error 반환
+        UserEntity userEntity = getUserEntityOrException(email);
+
+        ConstellationEntity constellationEntity = getConstellationEntityOrException(constellationId);
+
+        //TODO : 별자리가 NONSHARED
+        return articleRepository.findAllByConstellationEntity(constellationEntity, userEntity, pageable).map(Article::fromEntity);
     }
 
     // 포스트가 존재하는지
     private ArticleEntity getArticleEntityOrException(Long articleId) {
         return articleRepository.findById(articleId).orElseThrow(() ->
-                new ByeolDamException(ErrorCode.ARTICLE_NOT_FOUND, String.format("%s not founded", articleId)));
+                new ByeolDamException(ErrorCode.ARTICLE_NOT_FOUND, String.format("%s not founded", "articleId:" + Long.toString(articleId))));
     }
 
     // 유저가 존재하는지
     private UserEntity getUserEntityOrException(String email) {
         return userRepository.findByEmail(email).orElseThrow(() ->
-                new ByeolDamException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", email)));
+                new ByeolDamException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", "email:" +email)));
     }
 
+    // 별자리가 존재하는지
+    private ConstellationEntity getConstellationEntityOrException(Long constellationId) {
+        return constellationRepository.findById(constellationId).orElseThrow(() ->
+                new ByeolDamException(ErrorCode.CONSTELLATION_NOT_FOUND, String.format("%s not founded", "constellationId:" + Long.toString(constellationId))));
+    }
+
+    // 게시물 owner인지 확인
+    private ArticleEntity getArticleOwnerOrException(Long articleId, String email){
+        UserEntity userEntity = getUserEntityOrException(email);                                    // 현재 사용자 user entity
+        ArticleEntity articleEntity = getArticleEntityOrException(articleId);
+        UserEntity ownerEntity = articleEntity.getOwnerEntity();                              // admin의 user entity
+
+        // admin이어야 삭제 가능
+        if(ownerEntity != userEntity) {
+            throw new ByeolDamException(ErrorCode.INVALID_PERMISSION,
+                    String.format("%s has no permission with %s", "email:"+email, "articleId:" + Long.toString(articleId)));
+        }
+
+        return articleEntity;
+    }
 }
